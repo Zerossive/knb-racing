f18dde4e4b6b28968dc3a7e51a4c68ff3b1869b1
Zerossive <zerossive@gmail.com>
2021-08-20 16:18:41 -0500

    connected videos to CMS on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,8 +11,10 @@ export default function Home() {
 
     const [images, setImages] = useState([]);
     const [articles, setArticles] = useState([]);
+    const [videos, setVideos] = useState([]);
 
     useEffect(() => {
+        // Get carousel data
         client
             .getEntries({
                 content_type: "homeCarousel",
@@ -25,6 +27,7 @@ export default function Home() {
                 setImages(sortedItems);
             })
             .catch(console.error);
+        // Get articles
         client
             .getEntries({
                 content_type: "homeArticles",
@@ -39,6 +42,25 @@ export default function Home() {
                     return item;
                 });
                 setArticles(sortedItems);
+            })
+            .catch(console.error);
+        // Get videos
+        client
+            .getEntries({
+                content_type: "homeVideos",
+            })
+            .then((response) => {
+                // Sort and format rich text
+                let sortedItems = response.items.sort((a, b) => {
+                    return a.fields.index - b.fields.index;
+                });
+                sortedItems.map((item) => {
+                    item.fields.description = documentToHtmlString(
+                        item.fields.description
+                    );
+                    return item;
+                });
+                setVideos(sortedItems);
             })
             .catch(console.error);
     }, []);
@@ -83,7 +105,12 @@ export default function Home() {
                                             minHeight: "300px",
                                         }}
                                     />
-                                    <p className='legend'>{description}</p>
+                                    <p
+                                        className='legend'
+                                        style={{ opacity: "0.75" }}
+                                    >
+                                        {description}
+                                    </p>
                                 </div>
                             );
                         })}
@@ -126,129 +153,48 @@ export default function Home() {
                     );
                 })}
 
-                {/* Videos */}
                 <div className='w12 grid-item center-vertical'>
-                    {/* YT Video */}
-                    <div className='w6 pad'>
-                        <div className='w12 card'>
-                            {/* Title */}
-                            <h2 className='w12 title'>
-                                first time behind the wheel
-                            </h2>
-                            {/* Video */}
-                            <ReactPlayer
-                                url='https://www.youtube.com/watch?v=I0KIFlzVAHk'
-                                controls={true}
-                                light={true}
-                                playing={true}
-                                width='100%'
-                                height='auto'
-                                style={{ aspectRatio: "16/9" }}
-                            />
-                            {/* Description */}
-                            {/* <article className='w12 pad'>
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur
-                                    adipisicing elit. Aperiam ab libero iste
-                                    dolore ipsa pariatur dolores reiciendis
-                                    eveniet officiis enim.
-                                </p>
-                            </article> */}
-                        </div>
-                    </div>
+                    {/* Videos */}
+                    {videos.map((video) => {
+                        const { title, videoUrl, description, index } =
+                            video.fields;
+
+                        return (
+                            <div className='w6 pad' key={index}>
+                                <div className='w12 card'>
+                                    {/* Title */}
+                                    {title && (
+                                        <h2 className='w12 title'>{title}</h2>
+                                    )}
+                                    {/* Video */}
+                                    <ReactPlayer
+                                        url={videoUrl}
+                                        controls={true}
+                                        light={true}
+                                        playing={true}
+                                        width='100%'
+                                        height='auto'
+                                        style={{
+                                            aspectRatio: "16/9",
+                                            minHeight: "200px",
+                                        }}
+                                    />
+                                    {/* Description */}
+                                    {description && (
+                                        <article className='w12 pad'>
+                                            <p
+                                                dangerouslySetInnerHTML={{
+                                                    __html: description,
+                                                }}
+                                            ></p>
+                                        </article>
+                                    )}
+                                </div>
+                            </div>
+                        );
+                    })}
                 </div>
             </div>
-
-            {/* TESTING */}
-            {/* <div className='grid-container full-width'>
-                <div className='grid-item w12' style={{ background: "white" }}>
-                    <iframe
-                        width='auto'
-                        height='auto'
-                        title='youtube video'
-                        style={{
-                            border: "none",
-                            overflow: "hidden",
-                            // aspectRatio: "9/16",
-                            // minHeight: "700px",
-                            minHeight: "300px",
-                        }}
-                        className='w6'
-                        src='https://www.youtube.com/embed/I0KIFlzVAHk'
-                    ></iframe>
-                    <article className='w6 pad center-vertical'>
-                        <h2 className='title pad' style={{ color: "black" }}>
-                            Kaylee Boyce
-                        </h2>
-                        <p
-                            className='pad'
-                            style={{
-                                fontSize: "1.2rem",
-                                lineHeight: "2.4rem",
-                            }}
-                        >
-                            I was born on November 28, 2006 in Mesquite TX. I
-                            started cheer when i was 5 and stayed in it for 5
-                            years, when I wasn't recovering from a broken arm
-                            that is. A couple years later I broke my foot, then
-                            a few months after that I broke my thumb. Then we
-                            moved closer to The Devils Bowl dirt track and I
-                            could hear the cars racing while swimming in the
-                            pool at night. So I asked my Mom if I could go to a
-                            race and watch. She said yes, so at thirteen my dad
-                            finally took me to the Outlaw Race of 2020 and from
-                            there I was hooked on dirt track racing. That summer
-                            mom and dad bought me a tuner car. It was the best,
-                            dad and I started working on it to get it ready for
-                            the 2021 season and here I am today.
-                        </p>
-                    </article>
-                </div>
-            </div> */}
-
-            {/* TESTING */}
-            {/* <div className='grid-container full-width'>
-                <div className='grid-item w12' style={{ background: "white" }}>
-                    <img
-                        src={raceData[0].image1}
-                        alt='race image'
-                        className='w6'
-                        style={{
-                            objectFit: "cover",
-                            height: "auto",
-                            backgroundSize: "cover",
-                            objectPosition: "center",
-                        }}
-                    />
-                    <article className='w6 pad center-vertical'>
-                        <h1 className='title pad' style={{ color: "black" }}>
-                            Kaylee Boyce
-                        </h1>
-                        <p
-                            className='pad'
-                            style={{
-                                fontSize: "1.5rem",
-                                lineHeight: "3rem",
-                            }}
-                        >
-                            I was born on November 28, 2006 in Mesquite TX. I
-                            started cheer when i was 5 and stayed in it for 5
-                            years, when I wasn't recovering from a broken arm
-                            that is. A couple years later I broke my foot, then
-                            a few months after that I broke my thumb. Then we
-                            moved closer to The Devils Bowl dirt track and I
-                            could hear the cars racing while swimming in the
-                            pool at night. So I asked my Mom if I could go to a
-                            race and watch. She said yes, so at thirteen my dad
-                            finally took me to the Outlaw Race of 2020 and from
-                            there I was hooked on dirt track racing. That summer
-                            mom and dad bought me a tuner car. It was the best,
-                            dad and I started working on it to get it ready for
-                            the 2021 season and here I am today.
-                        </p>
-                    </article>
-                </div>
-            </div> */}
         </main>
     );
 }
